a0ab9b53b95d78997479fb4a57bc34332e70a9cb
pwood1284@gmail.com <your_email@whatever.com>
2015-06-09 07:28:54 -0400

    added html,css

--- lib/etsiest.rb
+++ lib/etsiest.rb
@@ -1,5 +1,21 @@
 require "etsiest/version"
+require "sinatra/base"
+require "pry"
+require 'etsy'
+
+
+  Etsy.api_key = ENV['ETSY_KEY']
+
 
 module Etsiest
-  # Your code goes here...
+  class Search < Sinatra::Base
+    get '/' do
+      @result = Etsy::Request.get('/listings/active',:includes => ['Images', 'Shop'],:keywords => 'whiskey')
+      # binding.pry
+      erb :index
+    end
+
+
+    run! if app_file == $0
+  end
 end
